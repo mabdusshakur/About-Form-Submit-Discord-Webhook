4d6db015dded3590e9fed2360c9eec7135dd6aad
M Abdus Shakur <mabdusshakur12@gmail.com>
2023-11-25 11:46:45 +0600

    Add project files.

---
+++ About Form Submit Discord Webhook/Main.Designer.cs
@@ -0,0 +1,266 @@
+﻿namespace About_Form_Submit_Discord_Webhook
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.siticoneDragControl1 = new Siticone.UI.WinForms.SiticoneDragControl(this.components);
+            this.siticoneControlBox1 = new Siticone.UI.WinForms.SiticoneControlBox();
+            this.siticoneControlBox2 = new Siticone.UI.WinForms.SiticoneControlBox();
+            this.name_tb = new Siticone.UI.WinForms.SiticoneTextBox();
+            this.siticoneLabel1 = new Siticone.UI.WinForms.SiticoneLabel();
+            this.siticoneLabel2 = new Siticone.UI.WinForms.SiticoneLabel();
+            this.phone_tb = new Siticone.UI.WinForms.SiticoneTextBox();
+            this.siticoneLabel3 = new Siticone.UI.WinForms.SiticoneLabel();
+            this.email_tb = new Siticone.UI.WinForms.SiticoneTextBox();
+            this.developer_cb = new Siticone.UI.WinForms.SiticoneCheckBox();
+            this.submit_btn = new Siticone.UI.WinForms.SiticoneButton();
+            this.cp_cb = new Siticone.UI.WinForms.SiticoneCheckBox();
+            this.SuspendLayout();
+            // 
+            // siticoneDragControl1
+            // 
+            this.siticoneDragControl1.TargetControl = this;
+            // 
+            // siticoneControlBox1
+            // 
+            this.siticoneControlBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.siticoneControlBox1.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(139)))), ((int)(((byte)(152)))), ((int)(((byte)(166)))));
+            this.siticoneControlBox1.HoveredState.Parent = this.siticoneControlBox1;
+            this.siticoneControlBox1.IconColor = System.Drawing.Color.White;
+            this.siticoneControlBox1.Location = new System.Drawing.Point(246, 3);
+            this.siticoneControlBox1.Name = "siticoneControlBox1";
+            this.siticoneControlBox1.ShadowDecoration.Parent = this.siticoneControlBox1;
+            this.siticoneControlBox1.Size = new System.Drawing.Size(45, 29);
+            this.siticoneControlBox1.TabIndex = 0;
+            // 
+            // siticoneControlBox2
+            // 
+            this.siticoneControlBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.siticoneControlBox2.ControlBoxType = Siticone.UI.WinForms.Enums.ControlBoxType.MinimizeBox;
+            this.siticoneControlBox2.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(139)))), ((int)(((byte)(152)))), ((int)(((byte)(166)))));
+            this.siticoneControlBox2.HoveredState.Parent = this.siticoneControlBox2;
+            this.siticoneControlBox2.IconColor = System.Drawing.Color.White;
+            this.siticoneControlBox2.Location = new System.Drawing.Point(195, 3);
+            this.siticoneControlBox2.Name = "siticoneControlBox2";
+            this.siticoneControlBox2.ShadowDecoration.Parent = this.siticoneControlBox2;
+            this.siticoneControlBox2.Size = new System.Drawing.Size(45, 29);
+            this.siticoneControlBox2.TabIndex = 1;
+            // 
+            // name_tb
+            // 
+            this.name_tb.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.name_tb.DefaultText = "";
+            this.name_tb.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.name_tb.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.name_tb.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.name_tb.DisabledState.Parent = this.name_tb;
+            this.name_tb.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.name_tb.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.name_tb.FocusedState.Parent = this.name_tb;
+            this.name_tb.HoveredState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.name_tb.HoveredState.Parent = this.name_tb;
+            this.name_tb.Location = new System.Drawing.Point(79, 66);
+            this.name_tb.Name = "name_tb";
+            this.name_tb.PasswordChar = '\0';
+            this.name_tb.PlaceholderText = "";
+            this.name_tb.SelectedText = "";
+            this.name_tb.ShadowDecoration.Parent = this.name_tb;
+            this.name_tb.Size = new System.Drawing.Size(200, 36);
+            this.name_tb.TabIndex = 2;
+            this.name_tb.Tag = "";
+            // 
+            // siticoneLabel1
+            // 
+            this.siticoneLabel1.BackColor = System.Drawing.Color.Transparent;
+            this.siticoneLabel1.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.siticoneLabel1.Location = new System.Drawing.Point(16, 71);
+            this.siticoneLabel1.Name = "siticoneLabel1";
+            this.siticoneLabel1.Size = new System.Drawing.Size(45, 17);
+            this.siticoneLabel1.TabIndex = 3;
+            this.siticoneLabel1.Text = "Name  : ";
+            // 
+            // siticoneLabel2
+            // 
+            this.siticoneLabel2.BackColor = System.Drawing.Color.Transparent;
+            this.siticoneLabel2.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.siticoneLabel2.Location = new System.Drawing.Point(13, 157);
+            this.siticoneLabel2.Name = "siticoneLabel2";
+            this.siticoneLabel2.Size = new System.Drawing.Size(52, 17);
+            this.siticoneLabel2.TabIndex = 5;
+            this.siticoneLabel2.Text = "Phone : ";
+            // 
+            // phone_tb
+            // 
+            this.phone_tb.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.phone_tb.DefaultText = "";
+            this.phone_tb.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.phone_tb.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.phone_tb.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.phone_tb.DisabledState.Parent = this.phone_tb;
+            this.phone_tb.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.phone_tb.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.phone_tb.FocusedState.Parent = this.phone_tb;
+            this.phone_tb.HoveredState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.phone_tb.HoveredState.Parent = this.phone_tb;
+            this.phone_tb.Location = new System.Drawing.Point(79, 150);
+            this.phone_tb.Name = "phone_tb";
+            this.phone_tb.PasswordChar = '\0';
+            this.phone_tb.PlaceholderText = "";
+            this.phone_tb.SelectedText = "";
+            this.phone_tb.ShadowDecoration.Parent = this.phone_tb;
+            this.phone_tb.Size = new System.Drawing.Size(200, 36);
+            this.phone_tb.TabIndex = 4;
+            this.phone_tb.Tag = "";
+            // 
+            // siticoneLabel3
+            // 
+            this.siticoneLabel3.BackColor = System.Drawing.Color.Transparent;
+            this.siticoneLabel3.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.siticoneLabel3.Location = new System.Drawing.Point(13, 114);
+            this.siticoneLabel3.Name = "siticoneLabel3";
+            this.siticoneLabel3.Size = new System.Drawing.Size(52, 17);
+            this.siticoneLabel3.TabIndex = 7;
+            this.siticoneLabel3.Text = "Email : ";
+            // 
+            // email_tb
+            // 
+            this.email_tb.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.email_tb.DefaultText = "";
+            this.email_tb.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.email_tb.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.email_tb.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.email_tb.DisabledState.Parent = this.email_tb;
+            this.email_tb.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.email_tb.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.email_tb.FocusedState.Parent = this.email_tb;
+            this.email_tb.HoveredState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.email_tb.HoveredState.Parent = this.email_tb;
+            this.email_tb.Location = new System.Drawing.Point(79, 108);
+            this.email_tb.Name = "email_tb";
+            this.email_tb.PasswordChar = '\0';
+            this.email_tb.PlaceholderText = "";
+            this.email_tb.SelectedText = "";
+            this.email_tb.ShadowDecoration.Parent = this.email_tb;
+            this.email_tb.Size = new System.Drawing.Size(200, 36);
+            this.email_tb.TabIndex = 6;
+            this.email_tb.Tag = "";
+            // 
+            // developer_cb
+            // 
+            this.developer_cb.AutoSize = true;
+            this.developer_cb.CheckedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.developer_cb.CheckedState.BorderRadius = 2;
+            this.developer_cb.CheckedState.BorderThickness = 0;
+            this.developer_cb.CheckedState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.developer_cb.Location = new System.Drawing.Point(16, 210);
+            this.developer_cb.Name = "developer_cb";
+            this.developer_cb.Size = new System.Drawing.Size(84, 17);
+            this.developer_cb.TabIndex = 8;
+            this.developer_cb.Text = "Developer ?";
+            this.developer_cb.UncheckedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(125)))), ((int)(((byte)(137)))), ((int)(((byte)(149)))));
+            this.developer_cb.UncheckedState.BorderRadius = 2;
+            this.developer_cb.UncheckedState.BorderThickness = 0;
+            this.developer_cb.UncheckedState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(125)))), ((int)(((byte)(137)))), ((int)(((byte)(149)))));
+            this.developer_cb.UseVisualStyleBackColor = true;
+            // 
+            // submit_btn
+            // 
+            this.submit_btn.CheckedState.Parent = this.submit_btn;
+            this.submit_btn.CustomImages.Parent = this.submit_btn;
+            this.submit_btn.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.submit_btn.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.submit_btn.ForeColor = System.Drawing.Color.White;
+            this.submit_btn.HoveredState.Parent = this.submit_btn;
+            this.submit_btn.Location = new System.Drawing.Point(0, 268);
+            this.submit_btn.Name = "submit_btn";
+            this.submit_btn.ShadowDecoration.Parent = this.submit_btn;
+            this.submit_btn.Size = new System.Drawing.Size(294, 45);
+            this.submit_btn.TabIndex = 9;
+            this.submit_btn.Text = "Submit";
+            this.submit_btn.Click += new System.EventHandler(this.submit_btn_Click);
+            // 
+            // cp_cb
+            // 
+            this.cp_cb.AutoSize = true;
+            this.cp_cb.CheckedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.cp_cb.CheckedState.BorderRadius = 2;
+            this.cp_cb.CheckedState.BorderThickness = 0;
+            this.cp_cb.CheckedState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.cp_cb.Location = new System.Drawing.Point(16, 233);
+            this.cp_cb.Name = "cp_cb";
+            this.cp_cb.Size = new System.Drawing.Size(94, 17);
+            this.cp_cb.TabIndex = 10;
+            this.cp_cb.Text = "Copy/Paster ?";
+            this.cp_cb.UncheckedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(125)))), ((int)(((byte)(137)))), ((int)(((byte)(149)))));
+            this.cp_cb.UncheckedState.BorderRadius = 2;
+            this.cp_cb.UncheckedState.BorderThickness = 0;
+            this.cp_cb.UncheckedState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(125)))), ((int)(((byte)(137)))), ((int)(((byte)(149)))));
+            this.cp_cb.UseVisualStyleBackColor = true;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(294, 313);
+            this.Controls.Add(this.cp_cb);
+            this.Controls.Add(this.submit_btn);
+            this.Controls.Add(this.developer_cb);
+            this.Controls.Add(this.siticoneLabel3);
+            this.Controls.Add(this.email_tb);
+            this.Controls.Add(this.siticoneLabel2);
+            this.Controls.Add(this.phone_tb);
+            this.Controls.Add(this.siticoneLabel1);
+            this.Controls.Add(this.name_tb);
+            this.Controls.Add(this.siticoneControlBox2);
+            this.Controls.Add(this.siticoneControlBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Main";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Siticone.UI.WinForms.SiticoneDragControl siticoneDragControl1;
+        private Siticone.UI.WinForms.SiticoneTextBox name_tb;
+        private Siticone.UI.WinForms.SiticoneControlBox siticoneControlBox2;
+        private Siticone.UI.WinForms.SiticoneControlBox siticoneControlBox1;
+        private Siticone.UI.WinForms.SiticoneLabel siticoneLabel3;
+        private Siticone.UI.WinForms.SiticoneTextBox email_tb;
+        private Siticone.UI.WinForms.SiticoneLabel siticoneLabel2;
+        private Siticone.UI.WinForms.SiticoneTextBox phone_tb;
+        private Siticone.UI.WinForms.SiticoneLabel siticoneLabel1;
+        private Siticone.UI.WinForms.SiticoneCheckBox cp_cb;
+        private Siticone.UI.WinForms.SiticoneButton submit_btn;
+        private Siticone.UI.WinForms.SiticoneCheckBox developer_cb;
+    }
+}
+
---
+++ About Form Submit Discord Webhook/Main.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace About_Form_Submit_Discord_Webhook
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+
+        private static byte[] sendData(string endPoint, NameValueCollection data)
+        {
+            using(WebClient wc = new WebClient())
+            {
+                return wc.UploadValues(endPoint, data);
+            }
+        }
+
+        private static void sendToDiscord(string url, string message, string botname)
+        {
+            sendData(url, new NameValueCollection()
+            {
+                {
+                    "username",
+                    botname
+                },
+                {
+                    "content",
+                    message
+                }
+            });
+        }
+
+        private void submit_btn_Click(object sender, EventArgs e)
+        {
+            string url = "https://discord.com/api/webhooks/1177846825206677534/CxUTnguTz0zROQoc9IH17Q2iC7g7qKIGhQHt_5NXFKgHgDbhA_uZYJHntCK2JmFc77Zx";
+            string Name = name_tb.Text;
+            string email = email_tb.Text;
+            string Phoen = phone_tb.Text;
+
+            bool developer = false ;
+            bool copy_paster = false;
+
+            if (developer_cb.Checked) { developer = true; cp_cb.Checked = false; }
+            if(cp_cb.Checked ) { copy_paster = true; developer_cb.Checked = false; }
+
+
+            var sendData = "Name : " + Name + "\nEmail : " + email + "\nPhoen : " + Phoen + "\n ```Developer : " + developer + "\nCopy Paster : " + copy_paster + "```";
+            sendToDiscord(url,sendData,"User Information Form");
+        }
+    }
+}
